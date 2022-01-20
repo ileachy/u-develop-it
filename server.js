c79e635202dd4d40097e24783fa5bb7aecf8e5c1
Ian L <ileach81@gmail.com>
2022-01-20 14:31:48 -0500

    set up node server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+// dependencie variables
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found)\
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Starts the express server on port 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
